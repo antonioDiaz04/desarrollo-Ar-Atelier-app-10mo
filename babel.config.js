d256d81e15892a18ed44568848afbd36bdba853d
antonioDiaz04 <20221136@uthh.edu.mx>
2025-09-25 09:46:44 -0600

    LEARNJIRA-14 <inicio de proyecto configurado>

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,8 @@
 module.exports = function (api) {
   api.cache(true);
-  let plugins = [];
+  let plugins = [
+      'react-native-worklets-core/plugin', // <-- Añade esta línea
+  ];
 
   return {
     presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel'],
